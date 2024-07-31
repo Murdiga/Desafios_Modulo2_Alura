29be70f397a7a4dc0e60573df3c503f873a9cee8
unknown <guimurdiga@hotmail.com>
2024-07-30 22:27:33 -0300

    Exibe a ficha do Aluno

---
+++ Desafio5/src/Aluno.java
@@ -0,0 +1,14 @@
+public class Aluno {
+
+    String nome;
+    int idade;
+
+    void fichaAluno() {
+
+        System.out.println("Nome do Aluno: " + nome +
+                "\n" +
+                "Idade do Aluno: " + idade);
+
+    }
+
+}
---
+++ Desafio5/src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[] args) throws Exception {
+
+        Aluno aluno = new Aluno();
+
+        aluno.nome = "Guilherme";
+        aluno.idade = 22;
+
+        aluno.fichaAluno();
+        
+    }
+}
